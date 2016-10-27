f7557201dd26980446c0a436e178a0e848c8f1b1
Adam Zhang <adambzhang@gmail.com>
2016-10-27 16:57:18 -0400

    Add files via upload

--- SISwithAwareness.py
+++ SISwithAwareness.py
@@ -2,14 +2,14 @@ import numpy as np
 
 network = [[0,1,0,0,0],
            [1,0,1,1,1],
-           [0,1,0,0,0],
-           [0,1,0,0,0],
-           [0,1,0,0,0]]
-
-state = [0,0,0,0,0]
-priorstate = [0,0,0,1,1]
-
-beta = 0.5 #infection rate
+           [0,1,0,0,0],                 #generalize to multiple nodes, storage not print, another python file with distancing model
+           [0,1,0,0,0],                 #random number of nodes and random states
+           [0,1,0,0,0]]                 #the network can be made manually
+                                        #bc symmetric networks i=j, j=i, then just iterate through half of the matrix i>j
+                                        #might need more functions for network generation
+priorstate = [0,1,0,0,0]                #look into making plots vs lists
+state = [0,1,0,0,0]
+beta = 0.3 #infection rate
 delta = 0.2 #healing rate
 
 #def SISwithAwareness():
@@ -31,7 +31,9 @@ for time in range(0,iteration):
             if delta > flip:    #flips to see if node heals
                 state[i] = 0
     print state
-    priostate = state
+    for element in range(0,5):
+        priorstate[element] = state[element]
+    
 
 #        for j in range(0,5):
 #            if network[i][j] == 0:
@@ -59,42 +61,6 @@ for time in range(0,iteration):
 
 
 
-#a=b=c=d=e=1
-#array1 = [[b],[a,c,d,e],[b],[b],[b]]
-#for a in (0,5):
-#    for b in (0,5):
-#        if array[a][b] == 1:
-#            array1[a][0] = array[a][b]
 
-
-#def SISwithAwareness():
-#    s_i = 0
-#    t = 1
-#    delta = np.random.uniform(0.0,1.0)  
-#    beta = np.random.uniform(0.0,0.5)   
-#    for time in range(0,t):
-#        for i in range(0,5):
-#            if  
-#                p_i = 1   #probability of staying healthy initially
-#                for a in range(0,5):
-#                    for b in range(0,5):
-#                        for array1[x][y] == 1: #np.apply_along_axis()
-#                            p = (1-beta)*p
-#                        p_i = (0,1) #probability of staying healthy in time t+1
-#        infected = np.random.random_integers(0,1)   #random number generator
-#        if pi-1 < infected & s_i == 0:
-#             s_t+1 = 1
-#        for i in range (1,n) & list[time]=1:
-#            if delta > infected:
-#        s(t+1) = 0
-#        for i in (0,5);
-#            for j in (0,5):
-#                if array[i][j] == 1:
-#
-#        print "a is" + a
-#        print "b is" + b
-#        print "c is" + c
-#        print "d is" + d
-#        print "e is" + e
 
 
