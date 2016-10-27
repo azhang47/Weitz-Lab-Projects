6821a21adcdc269df7450663e9020da0250fe1c6
Adam Zhang <adambzhang@gmail.com>
2016-10-27 02:33:35 -0400

    Add files via upload

--- SISwithAwareness.py
+++ SISwithAwareness.py
@@ -7,38 +7,55 @@ network = [[0,1,0,0,0],
            [0,1,0,0,0]]
 
 state = [0,0,0,0,0]
-priorstate = [0,0,0,0,0]
+priorstate = [0,0,0,1,1]
 
 beta = 0.5 #infection rate
-delta = 0.5 #healing rate
+delta = 0.2 #healing rate
 
 #def SISwithAwareness():
-iteration = 10
+iteration = 50
 print state
 for time in range(0,iteration):
     for i in range(0,5):
-        for j in range(0,5):
-            if network[i][j] == 0:
-                probability = 1
-                try:
-                    if network[i][j-1] == 1: 
-                        probability = (1-beta)*probability
-                except:
-                    pass
-                try:
-                    if network[i][j+1] == 1:
-                        probability = (1-beta)*probability #if multipe neighbors sick, higher chance of sickness
-                except:
-                    pass
+        if priorstate[i] == 0:  #check if node is healthy
+            probability = 1     #reset probability
+            for k in range(0,5):        
+                if network[i][k] == 1:  #iterate through its neighbors
+                    if priorstate[k] == 1:  #check if neighbor is infected
+                        probability = (1-beta)*probability  #infected neighbor increases chance of sickness
+                        flip = np.random.uniform(0.0,1.0)
+                        if probability < flip:
+                            state[i] = 1    #node is infected
+        if priorstate[i] == 1:  #checks if node is already infected
             flip = np.random.uniform(0.0,1.0)
-            if probability < flip:
-                if network[i][j] == 0:
-                    state[i] = 1
-            if network[i][j] == 1:
-                if delta > flip:
-                    state[i] = 0
+            if delta > flip:    #flips to see if node heals
+                state[i] = 0
     print state
-    priorstate = state
+    priostate = state
+
+#        for j in range(0,5):
+#            if network[i][j] == 0:
+#                probability = 1
+#                try:
+#                  if network[i][j-1] == 1: 
+#                        probability = (1-beta)*probability
+#                except:
+#                    pass
+#                try:
+#                   if network[i][j+1] == 1:
+#                        probability = (1-beta)*probability #if multipe neighbors sick, higher chance of sickness
+#                except:
+#                    pass
+
+#            flip = np.random.uniform(0.0,1.0)
+#            if probability < flip:
+#                if network[i][j] == 0:
+#                    state[i] = 1
+#            if network[i][j] == 1:
+#                if delta > flip:
+#                    state[i] = 0
+#    print state
+#    priorstate = state
 
 
 
